15dd1fbce8605d508dc2c43646aa7217fa286ed5
Indre <indre@outlook.com>
2021-03-07 20:49:13 +0200

    02 Calculator

---
+++ 02_Calculator/js/app.js
@@ -0,0 +1,13 @@
+/*
+ *
+ */
+
+function valueButton(e) {
+    calculatorForm.screen.value += e.value;
+}
+function Clear() {
+    calculatorForm.screen.value = null;
+}
+function Calculate() {
+    calculatorForm.screen.value = eval(calculatorForm.screen.value);
+}
